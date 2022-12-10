b81da1c9eb6cca5b4ce99b945ee1fc5a849a97a6
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-12-10 16:14:16 +0600

    search button style added

--- assets/common/adminNavbar.php
+++ assets/common/adminNavbar.php
@@ -24,7 +24,7 @@
 <nav>
   <ul>
     <li><input type="text" autocomplete="off" placeholder="Search user" name="search" id="search" value=""></li>
-    <li><input type="submit" name=searchBtn value="Search"></li>
+    <li><input type="submit" name=searchBtn value="Search" id="searchBtn"></li>
     <li><a href="../../views/admin/adminHome.php">Home</a></li>
     <li><a href="../../views/admin/menuAdmin.php">Menu</a></li>
     <li><a href="#">Stations</a></li>
