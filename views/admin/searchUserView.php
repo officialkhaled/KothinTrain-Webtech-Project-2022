c4bf94acc52509c732e85d1c4b1ee254199cac3b
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-12-13 21:05:35 +0600

    ...

--- views/admin/searchUserView.php
+++ views/admin/searchUserView.php
@@ -285,25 +285,6 @@
 
 </div>
 
-<!-- <script>
-  /* AJAX & JSON */
-  function Search() {
-    let search = document.getElementById('search').value;
-    let jsearch = {'search': search};
-    let json = JSON.stringify(jsearch);
-    let xhttp = new XMLHttpRequest();
-    xhttp.open('POST', '../../controllers/userListSearch.php', true);
-
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send('data='+json);
-    xhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200) {
-        document.getElementsByTagName('h3')[0].innerHTML = this.responseText;
-      }
-    }
-  }
-</script> -->
-
 </body>
 </html>
 
